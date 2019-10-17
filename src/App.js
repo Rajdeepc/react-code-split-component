3820036a34f067c7f8839f6eeb2407a7d369b563
Rajdeepc <rajrock38@gmail.com>
2019-10-17 22:11:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    modal: null
+  }
+
+  showLocation = () => {
+    import('./components/Modal')
+      .then((mod) => this.setState(() => ({
+        modal: mod.default
+      })
+    ))
+  }
+
+
+  render() {
+
+    const { modal: Modal } = this.state;
+
+    return (
+      <>
+      <div>Testing Code Splitting or Lazy loading of components</div>
+      <br></br>
+        {Modal !== null ?
+          <Modal />
+          :
+          <button onClick={this.showLocation}>
+            Show Modal
+          </button>
+        }
+      </>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Modal() {
+    return (
+        <div>
+            <h1>I am the modal which is loaded dynamically</h1>
+        </div>
+    )
+}
